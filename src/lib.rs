5af1debe1074b919ae77978175415607f228afea
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-05-23 18:15:35 +0200

    Use set_panic_hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,6 +10,8 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet() -> String{
+    set_panic_hook();
+    
     alert("Hello, test-wasm!");
     String::from("Hello, test-wasm!")
 }
